85ee4739c4a99e3ac51fe14b68229ea3c60c0fba
mesomatics <68718172+mesomatics@users.noreply.github.com>
2023-09-02 09:55:03 +0900

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import json
 PATH = os.getcwd()
 PATH_SONGS = os.path.join(PATH, "songs", "songs_all.json")
 PATH_IMAGES = os.path.join(PATH, "songs", "images")
-PATH_FONT = os.path.join(PATH, "font", "HMKMMAG.ttf")
+PATH_FONT = os.path.join(PATH, "font", "HMKMMAG.TTF")
 
 with open(PATH_SONGS, "r", encoding="utf-8") as s:
     SONGS = json.load(s)

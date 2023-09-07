62f2896c61eb1975e765e6cddd24bb52fb9e8f39
mesomatics <68718172+mesomatics@users.noreply.github.com>
2023-09-07 22:52:06 +0900

    Update front.py

--- front.py
+++ front.py
@@ -100,9 +100,9 @@ def update_levels(template, mode):
             options = list(range(10, 29))
     else:
         if mode == "Single":
-            options = []
+            options = [23]
         else:
-            options = [21, 23, 24]
+            options = [21, 22, 23, 24]
     return options
 
 @app.callback(
